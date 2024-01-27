8765616e2f52adc0b6888f7a2ee6542cd3251cc1
MiaoJingyi <miaojingyi86@gmail.com>
2024-01-28 01:08:06 +0800

    .

--- main.go
+++ main.go
@@ -1,20 +1,34 @@
 package main
 
 import (
-	"fmt"
 	ffmpeg "github.com/u2takey/ffmpeg-go"
+	"io/fs"
 	"log"
+	"path/filepath"
+	"strings"
 )
+
+func mp4ToGif(root string) error {
+	return filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !strings.EqualFold(filepath.Ext(path), ".mp4") {
+			return nil
+		}
+
+		return ffmpeg.Input(path).
+			Output(strings.ReplaceAll(path, ".mp4", ".gif"), ffmpeg.KwArgs{"s": "1280x720", "r": "12"}).
+			OverWriteOutput().ErrorToStdOut().Run()
+	})
+}
 
 func main() {
 	log.Println("Process Started ...")
 
-	filename := "009"
-	err := ffmpeg.Input(fmt.Sprintf("./%s.mp4", filename)).
-		Output(fmt.Sprintf("./%s.gif", filename), ffmpeg.KwArgs{"s": "1280x720", "r": "12"}).
-		OverWriteOutput().ErrorToStdOut().Run()
-	if err != nil {
-		log.Fatalf("ERROR :: %s \n", err.Error())
+	if err := mp4ToGif("assets"); err != nil {
+		log.Fatalf("Fatal ERROR :: %s \n", err.Error())
 	}
 	log.Println("Process Finished ...")
 }
