1d6f2a27bba09953dd06d9a28f0959837986cbf8
MiaoJingyi <miaojingyi86@gmail.com>
2024-01-27 23:23:10 +0800

    .

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	ffmpeg "github.com/u2takey/ffmpeg-go"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello")
+	log.Println("Process Started ...")
+
+	filename := "002"
+	err := ffmpeg.Input(fmt.Sprintf("./%s.mp4", filename)).
+		Output(fmt.Sprintf("./%s.gif", filename), ffmpeg.KwArgs{"r": "12"}).
+		OverWriteOutput().ErrorToStdOut().Run()
+	if err != nil {
+		log.Fatalf("ERROR :: %s \n", err.Error())
+	}
+	log.Println("Process Finished ...")
 }
