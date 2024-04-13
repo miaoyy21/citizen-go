f8346ddb379d7059310f1bfe5e14bc0811082b49
MiaoJingyi <miaojingyi86@gmail.com>
2024-04-14 04:43:25 +0800

    .

--- conf/mate_enum.go
+++ conf/mate_enum.go
@@ -3,7 +3,7 @@ package conf
 type MateCategory int
 
 var (
-	MateCategoryMineralOriginal MateCategory = 11 // 原矿石
-	MateCategoryMineralSemi     MateCategory = 12 // 半成品矿石
-	MateCategoryMineralFinished MateCategory = 13 // 成品矿石
+	MateCategoryMineralOriginal MateCategory = 1 // 原矿石
+	MateCategoryMineralSemi     MateCategory = 2 // 半成品矿石
+	MateCategoryMineralFinished MateCategory = 3 // 成品矿石
 )
